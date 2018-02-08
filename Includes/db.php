fa8cc6a47820bd03d276f4dae9a02392d6881a71
gadget228 <bastiaan228@live.nl>
2018-02-08 13:29:43 +0100

    0.1.2 db.php aangepast

--- Includes/db.php
+++ Includes/db.php
@@ -1,4 +1,11 @@
 <?php
-
-
+	$hostname = "localhost";
+	$database = "groome1q_PM";
+	$username = "groome1q_stg_grp";
+	$password = "!polkadot1950";
+	date_default_timezone_set('Europe/Amsterdam');
+	$PM = mysqli_connect($hostname, $username, $password);
+	if (!$PM) {
+		trigger_error(mysqli_error($PM),E_USER_ERROR); 
+	}
 ?>
