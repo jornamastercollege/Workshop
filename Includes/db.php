e4cc0596dfa1fb8780abc1aedd98495a244cbd10
vaporlain <andres.developer101@gmail.com>
2018-02-19 11:44:05 +0100

    AAAAAAAAAAAAAAAAAAAAAA

--- Includes/db.php
+++ Includes/db.php
@@ -3,6 +3,7 @@
 	$database = "imrebzb198_Workshopinschrijving";
 	$username = "imrebzb198_workshop";
 	$password = "Webmaster2018";
+	//zo dit is wel veilig denkek
 	date_default_timezone_set('Europe/Amsterdam');
 	$PM = mysqli_connect($hostname, $username, $password);
 ?>
