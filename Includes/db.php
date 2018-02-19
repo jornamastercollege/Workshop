188fc9040f9d61959022309ffdac38a4b275e14a
tibtebrummen <imre.boersma@gmail.com>
2018-02-19 11:53:19 +0100

    HELLO DARKNESS

--- Includes/db.php
+++ Includes/db.php
@@ -3,11 +3,6 @@
 	$database = "imrebzb198_Workshopinschrijving";
 	$username = "imrebzb198_workshop";
 	$password = "Webmaster2018";
-<<<<<<< HEAD
-	$port = "3306";
-=======
-	//zo dit is wel veilig denkek
->>>>>>> e4cc0596dfa1fb8780abc1aedd98495a244cbd10
 	date_default_timezone_set('Europe/Amsterdam');
 	$PM = mysqli_connect($hostname, $username, $password, $database, $port);
 ?>
