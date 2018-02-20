3ea86e33f21232c65e0d44c77cbb57279a58bd6a
tibtebrummen <imre.boersma@gmail.com>
2018-02-20 13:46:30 +0100

    sfkjdksdfakl;

--- Includes/db.php
+++ Includes/db.php
@@ -3,7 +3,6 @@
 	$database = "imrebzb198_Workshopinschrijving";
 	$username = "imrebzb198_workshop";
 	$password = "Webmaster2018";
-	$port = "3306";
 	date_default_timezone_set('Europe/Amsterdam');
-	$PM = mysqli_connect($hostname, $username, $password, $database, $port);
+	$PM = mysqli_connect($hostname, $username, $password, $database);
 ?>
