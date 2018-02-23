5a1042f5f347853c7c55e80a6336409841569cd7
tibtebrummen <imre.boersma@gmail.com>
2018-02-23 14:32:02 +0100

    wow andres is hard aan t werk vandaag

--- pages/OverzichtDocent.php
+++ pages/OverzichtDocent.php
@@ -16,31 +16,34 @@ $Username = "Gebruiker";
 
         <head>
             <!-- Favicon -->
-            <link href="../Workshop/img/Astrum_logo.jpg" rel="shortcut icon" type="image/vnd.microsoft.icon" style="background: #fff" />
+            <link href="../Workshop/img/Astrum_logo.jpg" rel="shortcut icon" type="image/vnd.microsoft.icon"/>
         </head>
 
         <body style="background-color: #333e42">
         
             <!-- Navbar -->
-            <nav class="navbar navbar-light navbar-toggleable-md" style="background-color: #1ca382">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon" />
-                </button>
-                <a class="navbar-brand">
-                    <img src="../img/Astrum.png" width="150" height="36" class="d-inline-block align-top" alt="aaaaaa">
-                </a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <h4>Welkom <?php echo $_SESSION["login_naam"]; ?>&nbsp;</h4>
-                    <button class="btn btn-secondary"><a href="../includes/logout.php">Uitloggen</a></button>
-                </div>
-            </nav>
+            <nav class="navbar navbar-toggleable-md bg-faded" style="background-color: #1ca382">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">
+  <img href=""></img>
+  </a>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-item" href="#">Docenten overzicht <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+    <p>Welkom <?php echo $_SESSION["login_naam"]; ?>&nbsp;<button class="btn btn-secondary" >Loguit</button></p>
+    
+    </span>
+  </div>
+</nav>
             <div class="container" style="background-color: white;">
                 <div class="row">
                     <div class="col-md-3 align-self-end" align="right">
-                    </br>
-                        <!--<input type="search" class="form-control" placeholder="Search...">-->
                     </div>
                 </div>
                 <div class="row">
@@ -52,18 +55,18 @@ $Username = "Gebruiker";
                         </thead>
                         <tbody>
                             <?php
-                                $sql = "SELECT 'ronde.Nummer' AS Ronde, student.Voornaam AS Voornaam, student.Achternaam AS Achternaam, workshop.Naam AS Workshop, workshopronde.* FROM workshop LEFT JOIN workshopronde ON workshopronde.WorkShopID = workshop.ID LEFT JOIN ronde ON workshopronde.RondeID = ronde.ID LEFT JOIN studentinschrijving ON studentinschrijving.WorkShopRondeID = workshopronde.ID LEFT JOIN student ON studentinschrijving.StudentID = student.ID WHERE studentinschrijving.StudentID IS NOT NULL";
+                           
+                                $sql = "SELECT ronde.Nummer AS Ronde, student.Voornaam AS Voornaam, student.Achternaam AS Achternaam, workshop.Naam AS Workshop, workshopronde.* FROM workshop LEFT JOIN workshopronde ON workshopronde.WorkShopID = workshop.ID LEFT JOIN ronde ON workshopronde.RondeID = ronde.ID LEFT JOIN studentinschrijving ON studentinschrijving.WorkShopRondeID = workshopronde.ID LEFT JOIN student ON studentinschrijving.StudentID = student.ID WHERE studentinschrijving.StudentID IS NOT NULL";
                                 $result = mysqli_query($PM, $sql) or die(mysqli_error());
-                                $row = mysqli_fetch_array($result)
-                                //while($row = mysqli_fetch_array($result)) {
+                                while($row = mysqli_fetch_array($result)) {
                             ?>
                                     <tr>
-                                        <td><? echo $row['Ronde']; ?></td>
-                                        <td><? echo $row['Workshop']; ?></td>
-                                        <td><? echo $row['Voornaam']. $row['Achternaam']; ?></td>
+                                        <td><?php echo $row['Voornaam']. " " .$row['Achternaam']; ?></td>
+                                        <td><?php echo $row['Workshop']; ?></td>
+                                        <td><?php echo $row['Ronde']; ?></td>
                                     </tr>
                                     <?php
-                                //}
+                                }
                             ?>
                         </tbody>
                     </table>
