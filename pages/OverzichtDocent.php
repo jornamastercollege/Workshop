a53ceeea51fa1b67bcbb2bb387b0547addc09c4a
gadget228 <bastiaan228@live.nl>
2018-03-08 15:35:41 +0100

    commit.exe

--- pages/OverzichtDocent.php
+++ pages/OverzichtDocent.php
@@ -5,6 +5,16 @@ $Username = "Gebruiker";
 if ($_SESSION['logged'] == false) {
     echo("<script>location.href='../index.php';</script>");
 }
+
+
+
+
+
+$file="demo.xls";
+$test="<table  ><tr><td>Cell 1</td><td>Cell 2</td></tr></table>";
+header("Content-type: application/vnd.ms-excel");
+header("Content-Disposition: attachment; filename=$file");
+echo $test;
 ?>
     <html>
 
