028eba83d4eabf1db1de29e6ad86f42a0226b500
vaporlain <andres.developer101@gmail.com>
2018-03-09 08:56:17 +0100

    OMG ITSA WORKS ISTSTS WORKS111!!!111111!!!

--- pages/OverzichtDocent.php
+++ pages/OverzichtDocent.php
@@ -31,7 +31,38 @@ if ($_SESSION['logged'] == false) {
         </head>
 
         <body style="background-color: #333e42">
+
+        <script> 
+        //Here comes some Jquery
+
+          $(document).ready(function () {
+              $('#example').dataTable({
+                  "sDom": 'T<"clear">lfrtip',
+                  "oTableTools": {
+                      "sSwfPath": "/swf/copy_cvs_xls_pdf.swf"
+                  }
+              });
+          });
         
+        </script>
+
+        <script>
+        var tableToExcel = (function() {
+  var uri = 'data:application/vnd.ms-excel;base64,'
+    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
+    , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+    , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+  return function(table, name) {
+    if (!table.nodeType) table = document.getElementById(table)
+    var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+    window.location.href = uri + base64(format(template, ctx))
+  }
+})()
+</script>
+
+
+
+
                         <!-- NAVBAR -->
                         <nav class="navbar navbar-light navbar-toggleable-md" style="background-color: #1ca382">
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -55,13 +86,14 @@ if ($_SESSION['logged'] == false) {
             <!-- ./NAVBAR -->
             <div class="container" style="background-color: white;">
                 <div class="row">
-                    <p>Export this to excel:</p> <div class="col-md-2 align-self-end" align="left">
-                    <button type="submit" class="btn-primay Download"><i class="fas fa-download"></i> </button>
+                    <div class="col-md-2 align-self-end" align="left">
+                    <input type="button" onclick="tableToExcel('testTable', 'W3C Example Table')" value="Export to Excel">
+                        
                     </div>
                 </div>
                 <div class="row">
                 <div class="tabledownload">
-                    <table class="table table-responsive col-md-12 table-striped">
+                    <table id="testTable" class="table table-responsive col-md-12 table-striped">
                         <thead>
                             <th>Leerling</th>
                             <th>Workshop</th>
