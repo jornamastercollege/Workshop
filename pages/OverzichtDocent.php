fd5cc154c31b97a1ce33b8aa15d749210885554e
gadget228 <bastiaan228@live.nl>
2018-03-09 09:04:23 +0100

    :)

--- pages/OverzichtDocent.php
+++ pages/OverzichtDocent.php
@@ -10,11 +10,11 @@ if ($_SESSION['logged'] == false) {
 
 
 
-$file="demo.xls";
+/*$file="demo.xls";
 $test="<table  ><tr><td>Cell 1</td><td>Cell 2</td></tr></table>";
 header("Content-type: application/vnd.ms-excel");
 header("Content-Disposition: attachment; filename=$file");
-echo $test;
+echo $test; */
 ?>
     <html>
 
