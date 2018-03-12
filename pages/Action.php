14a2b3eca73a197fc6d320a1f7b397f732872b24
gadget228 <bastiaan228@live.nl>
2018-03-12 11:56:23 +0100

    :)

--- pages/Action.php
+++ pages/Action.php
@@ -15,22 +15,25 @@
     $rInput = $_POST["rondeselect"];
     $studentID = $_SESSION["ID"];
 
-    $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
+    echo $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
     $result = mysqli_query($PM, $SQL);
     $row = mysqli_fetch_assoc($result);    
     $wrID = $row['ID'];
 
-    $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
+    echo $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
     mysqli_query($PM, $SQL2);
 
-    $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = '$wrID'";
+    echo $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = '$wrID'";
     mysqli_select_db($PM, $database);
     $result3 = mysqli_query($PM, $SQL3);
     $row3 = mysqli_fetch_array($result3);
     $currentaantal = $row3['CurrentDeeln'] + 1;
-    $SQL4 = "UPDATE workshop SET CurrentDeeln = '$currentaantal' WHERE ID = '$wsInput'";
+    echo $SQL4 = "UPDATE workshop SET CurrentDeeln = '$currentaantal' WHERE ID = '$wsInput'";
     $result4 = mysqli_query($PM, $SQL4);
-
+    echo $SQL. "<br>";
+    echo $SQL2. "<br>";
+    echo $SQL3. "<br>";
+    echo $SQL4. "<br>";
     if (!$result4)
     {
         echo "ERROR:". mysqli_error($PM);
--- pages/OverzichtLeerling.php
+++ pages/OverzichtLeerling.php
@@ -15,15 +15,15 @@
         $rInput = $_POST["rondeselect"];
         $studentID = $_SESSION["ID"];
 
-        $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
+        echo $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
         $result = mysqli_query($PM, $SQL);
         $row = mysqli_fetch_assoc($result);    
         $wrID = $row['ID'];
     
-        $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
+        echo $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
         mysqli_query($PM, $SQL2);
 
-        $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = '$wrID'";
+       echo $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = '$wrID'";
         mysqli_select_db($PM, $database);
         $result3 = mysqli_query($PM, $SQL3);
         $row3 = mysqli_fetch_array($result3);
@@ -108,7 +108,7 @@
                     <i>
                      <p style="color:red; text-align: center;">Na het inschrijven voor een workshop kan deze niet meer worden aangepast!!</p>
                      <p style="color:red; text-align: center;">Je moet je inschrijven voor 2 workshops!</p>
-                     
+
                 </i>
                 <br>
 
