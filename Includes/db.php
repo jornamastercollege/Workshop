8e0b57be5d0100d73baf0b80406c09c834d8b1db
tibtebrummen <imre.boersma@gmail.com>
2018-03-12 12:53:33 +0100

    db

--- Includes/db.php
+++ Includes/db.php
@@ -1,8 +1,8 @@
 <?php
 	$hostname = "localhost";
 	$database = "imrebzb198_workshopinschrijving";
-	$username = "imrebzb198_workshop";
-	$password = "rT8%92";
+	$username = "root";
+	$password = "";
 	$port = "3306";
 	date_default_timezone_set('Europe/Amsterdam');
 	$PM = mysqli_connect($hostname, $username, $password, $database);
