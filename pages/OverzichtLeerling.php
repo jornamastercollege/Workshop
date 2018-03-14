a174894b2db53f3d2e082b361ae2d1fd08b8f39f
vaporlain <andres.developer101@gmail.com>
2018-03-14 15:44:17 +0100

    Talk Javascript to me baby

--- pages/OverzichtLeerling.php
+++ pages/OverzichtLeerling.php
@@ -34,6 +34,7 @@
     <html>
 
     <head>
+    <script src="jquery.js"/> </script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"
             crossorigin="anonymous">
         <link href="style.css" />
@@ -42,7 +43,6 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
             crossorigin="anonymous"></script>
 
-        <script src="../Includes/jquery.js">
         </script>
 
         <title>HealthEvent -
