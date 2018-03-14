cb786ef1d251e5990543821d4cda2828e8f1e0cd
tibtebrummen <imre.boersma@gmail.com>
2018-03-14 15:09:18 +0100

    v2.2

--- pages/Action.php
+++ pages/Action.php
@@ -6,27 +6,24 @@
  if ($_SESSION['logged'] == false) {
      echo("<script>location.href='../index.php';</script>");
  }
-
-   #Andres: ik ga een overzicht maken van de workshops waar lerling ingeschreven staat
+ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $wsInput = $_POST["workshopselect"];
-    $rInput = $_POST["rondeselect"];
-    $studentID = $_SESSION["ID"];
+    $wsInput2 = $_POST["workshopselect2"];
+    $studentID = $_SESSION['ID'];
 
-    echo $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
-    $result = mysqli_query($PM, $SQL);
-    $row = mysqli_fetch_assoc($result);    
-    $wrID = $row['ID'];
+    $insertInschrijving = "INSERT INTO `studentinschrijving` (`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wsInput)";
+    $insertInschrijving2 = "INSERT INTO `studentinschrijving` (`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wsInput2)";
 
-    echo $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
-    mysqli_query($PM, $SQL2);
-
-    $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = $wrID";
     mysqli_select_db($PM, $database);
-    $result3 = mysqli_query($PM, $SQL3);
-    $row3 = mysqli_fetch_array($result3);
-    $currentaantal = $row3['CurrentDeeln'] + 1;
-    echo $SQL4 = "UPDATE workshop SET CurrentDeeln = '$currentaantal' WHERE ID = '$wsInput'";
-    $result4 = mysqli_query($PM, $SQL4);
-header("Location: OverzichtLeerling.php");
+
+    $inschrijvingResult = mysqli_query($PM, $insertInschrijving);
+    $inschrijvingResult2 = mysqli_query($PM, $insertInschrijving2);
+
+    if (!$inschrijvingResult || !$inschrijvingResult2) {
+        echo mysqli_error($PM);
+    } else {
+        header("Location: OverzichtLeerling.php");
+    }
+}
 ?>
--- pages/OverzichtLeerling.php
+++ pages/OverzichtLeerling.php
@@ -11,28 +11,21 @@
     if($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $wsInput = $_POST["workshopselect"];
-        $rInput = $_POST["rondeselect"];
-        $studentID = $_SESSION["ID"];
+        $wsInput2 = $_POST["workshopselect2"];
+        $studentID = $_SESSION['ID'];
 
-        echo $SQL = "SELECT * FROM workshopronde WHERE rondeID = $rInput AND workshopID = $wsInput";
-        $result = mysqli_query($PM, $SQL);
-        $row = mysqli_fetch_assoc($result);
-        $wrID = $row['ID'];
-    
-        echo $SQL2 = "INSERT INTO `studentinschrijving`(`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wrID)";
-        mysqli_query($PM, $SQL2);
+        $insertInschrijving = "INSERT INTO `studentinschrijving` (`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wsInput)";
+        $insertInschrijving2 = "INSERT INTO `studentinschrijving` (`StudentID`, `WorkShopRondeID`) VALUES ($studentID, $wsInput2)";
 
-       echo $SQL3 = "SELECT ID, Naam, Omschrijving, MaxDeelnemers, CurrentDeeln FROM workshop WHERE ID = '$wrID'";
         mysqli_select_db($PM, $database);
-        $result3 = mysqli_query($PM, $SQL3);
-        $row3 = mysqli_fetch_array($result3);
-        $currentaantal=  $row3['CurrentDeeln'] + 1;
-        $SQL4 = "UPDATE workshop SET CurrentDeeln = '$currentaantal' WHERE ID = '$wsInput'";
-        $result4 = mysqli_query($PM, $SQL4);
 
-        if (!$result4)
-        {
-            echo "ERROR:". mysqli_error($PM);
+        $inschrijvingResult = mysqli_query($PM, $insertInschrijving);
+        $inschrijvingResult2 = mysqli_query($PM, $insertInschrijving2);
+
+        if (!$inschrijvingResult || !$inschrijvingResult2) {
+            echo mysqli_error($PM);
+        } else {
+            header("Location: OverzichtLeerling.php");
         }
     }
 
@@ -223,7 +216,7 @@ while($row = mysqli_fetch_array($result)) { //'for each' result
                                         <th>Ronde</th>
                                         <th>Ronde Tijd</th>
 
-                                        <!-- Hier zou ook moeten komen of er nog plek is of niet! -->
+                               
 
                                     </thead>
 
